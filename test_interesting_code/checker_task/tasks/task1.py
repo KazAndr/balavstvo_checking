3903fab30ab38278c42b23d4f44b08b035cf9895
Andrey Kazantsev <kaz.prao@bk.ru>
2019-12-03 19:30:25 +0300

    Update task1.py
    Some addition errors(wrong style comments, wrong style import, white spases) were added.

--- test_interesting_code/checker_task/tasks/task1.py
+++ test_interesting_code/checker_task/tasks/task1.py
@@ -1,5 +1,13 @@
-a = int(input())
-b = int(input())
-c = int(input())
+import os, sys
+
+#lfkdjsflkjlk
+#dmlfsklfsdf
+#ваыуаыауаыва
+a=int(input())
+b=int(input())
+c=int(input())
+if True:
+   sfdfs = 5
+sdf = k
 
 print (a + b + c)
